9686f27b2a6054c63c390160399593c575b3fb33
lburnell <l.burnell@talentticker.ai>
2021-06-04 10:42:10 +0100

    github menu addition

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -13,6 +13,9 @@ export default function Menu({ menuOpen, setMenuOpen }) {
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#contact">Contact</a>
         </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="https://www.github.com/leeburnell">Github</a>
+        </li>
       </ul>
     </div>
   );
